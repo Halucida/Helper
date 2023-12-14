6d2cd785e1f271e68f7643a88872a0f0a89cb341
Kepin <141326351+Internite@users.noreply.github.com>
2023-12-15 02:27:15 +0700

    Hii !

--- Hani/Helper.py
+++ Hani/Helper.py
@@ -1,3 +1,16 @@
 import pandas
 
-duplicated = lambda data, i : data.shape[0] != data[i].nunique()
+def duplicated(table, i):
+    if table.shape[0] == table[i].nunique():
+        return "All Good !"
+    result = table[table.duplicated(subset=i)]
+    return result
+
+def nullities(table):
+    null_i = table.isnull().sum()
+    if null_i.sum() == 0:
+        return 0
+    null_i = null_i[null_i != 0]
+    null_i = null_i.to_frame()
+    null_i.columns = ["Missing"]
+    return null_i
